60afb430734de0082777839b514fd17a2952b974
Luke Lighthart <Cu-Sith@gmx.com>
2021-04-28 19:53:18 -0400

    Added new dependency and login templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
-eng = create_engine("mssql+pyodbc://<username>:<password>@<freetds_name>/?charset=utf8")
+app = Flask(__name__, template_folder='templates')
+eng = create_engine("mssql+pyodbc://scott:tiger@mssql2017:1433/test?driver=ODBC+Driver+17+for+SQL+Server")
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("auth/login.html", title="Login")
 
 if __name__ == "__main__":
     app.run(debug=True)
